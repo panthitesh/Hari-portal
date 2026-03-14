545ea551ea06c15d23b32bf547a3d37ecf034e8b
Ubuntu <ubuntu@ip-172-31-31-78.ec2.internal>
2026-03-14 20:45:33 +0000

    Attendence portal project deployed on AWS EC2

---
+++ app/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+ res.send('Attendance Portal Running 🚀');
+});
+app.listen(port, '0.0.0.0', () => {
+ console.log(`Server running on port ${port}`);
+});
